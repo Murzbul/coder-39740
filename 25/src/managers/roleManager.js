074ebeeec27aaed46b879c18278cf2ea017cd823
nrusso <nrusso@digichanges.com>
2023-05-30 22:41:52 -0300

    feat: add class 25

---
+++ 25/src/managers/roleManager.js
@@ -0,0 +1,36 @@
+import RoleMongooseDao from "../daos/roleMongooseDao.js";
+
+class RoleManager
+{
+  constructor()
+  {
+     this.roleDao = new RoleMongooseDao();
+  }
+
+  async paginate(criteria)
+  {
+    return this.roleDao.paginate(criteria);
+  }
+
+  async getOne(id)
+  {
+    return this.roleDao.getOne(id);
+  }
+
+  async create(data)
+  {
+    return await this.roleDao.create(data);
+  }
+
+  async updateOne(id, data)
+  {
+    return this.roleDao.updateOne(id, data);
+  }
+
+  async deleteOne(id)
+  {
+    return this.roleDao.deleteOne(id);
+  }
+}
+
+export default RoleManager;
